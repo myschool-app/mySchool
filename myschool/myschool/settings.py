3601376c80d1aec69d1719000fb47cbe48d83abb
Afonso Santos <afonso.faisca1@gmail.com>
2019-12-03 20:54:09 +0000

    Inicío da tradução da aplicação

--- myschool/myschool/settings.py
+++ myschool/myschool/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -57,6 +58,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -118,7 +120,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'pt-PT'
+LANGUAGES = [
+    ('pt-PT', _('Português')),
+    ('en', _('English')),
+]
+
+LANGUAGE_CODE = 'en'
 
 TIME_ZONE = 'UTC'
 
@@ -151,3 +158,8 @@ LOGOUT_REDIRECT_URL = 'publico-inicio'
 # EMAIL_USE_TLS = True
 # EMAIL_HOST_USER = 'afonso.faisca1@gmail.com'
 # EMAIL_HOST_PASSWORD = 'qaaoahcdjmgwfsqb'
+
+# Tradução
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, 'locale'),
+]
