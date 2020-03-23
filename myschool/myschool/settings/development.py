ca12849e25e6ead0b867c0e9d65248e813b0db7a
Afonso Santos <afonso.faisca1@gmail.com>
2020-03-23 00:57:08 +0000

    Atualizado .gitignore

--- myschool/myschool/settings/development.py
+++ myschool/myschool/settings/development.py
@@ -31,5 +31,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'apikey'
-EMAIL_HOST_PASSWORD = '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'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
